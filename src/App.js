730d3713c80301298d5eebe32e5173ccb4c38cbd
kaju111 <saikiakaju275@gmail.com>
2023-06-08 21:48:51 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import Navber from "./components/Navber";
 import TextFrom from "./components/TextFrom";
+import About from "./components/About";
 
 function App() {
+
+  const [mode, setMode] = useState ("light")
+
+  const [text, setText] = useState ("Enable light mode")
+  
+
+
+  const toggleMode = () =>{
+    if(mode === "light"){
+    setMode ("dark")
+    setText("Enable light mode")
+    document.body.style.backgroundColor = "gray";
+    // document.text.style.color = "white";
+      
+  }
+   else{
+    setMode  ("light")
+    setText("Enable dark mode")
+    document.body.style.backgroundColor = "white";
+    
+  }  
+}
   return (
     <div className="App">
-     <Navber title="TextUtils"/>
-     <TextFrom heading="Enter the text analyze below"/>
+     <Navber title="TextUtils" mode={mode} toggleMode={toggleMode}  text={text} />
+     <TextFrom heading="Enter the text analyze below" mode={mode}/>
+     {/* <About toggleMode={toggleMode} mode={mode}/> */}
     </div>
   );
 }
--- src/components/TextFrom.jsx
+++ src/components/TextFrom.jsx
@@ -1,36 +1,82 @@
-import React from 'react'
-import { useState } from 'react' 
+import React, { useEffect } from "react";
+import { useState } from "react";
 
 function TextFrom(props) {
-  
-  const [text, setText] = useState("Text area change")
+  const [text, setText] = useState(" ");
 
-  const onChangeHandler = (event) =>{
-   
-      setText(event.target.value)
-  }
+  const onChangeHandler = (event) => {
+    setText(event.target.value);
+  };
 
-  const onClickHandler = () =>{
+  const onClickHandler = () => {
     let newText = text.toUpperCase();
-    setText(newText)
-    setButton("convert to lowercase")
+    setText(newText);
+  };
+  const ClickHandler = () => {
+    let newText = text.toLowerCase();
+    setText(newText);
+  };
+  const ClearHandler = () => {
+    let newText = ("");
+    setText(newText);
   }
 
+  
 
-  const[button, setButton] =useState ("Convert to uppercase")
-
-
+  useEffect(() => {
+    console.log("ho geya");
+  });
 
   return (
-    <div className='container my-3'>
-    <h1>{props.heading}</h1>
-    <div class="mb-3">  
-    <textarea class="form-control" value={text} onChange={onChangeHandler} id="myBox" rows="8"></textarea>
-</div>
+    <>
+      <div className="container my-3"  style={{color: props.mode === "dark" ? "white" : "black"}}>
+        <h1>{props.heading}</h1>
+        <div className="mb-3">
+          <textarea
+            style={{backgroundColor: props.mode === "dark" ? "gray" : "white",
+          color:props.mode === "dark" ? "white" : "black"}}
+            className="form-control"
+            value={text}
+            onChange={onChangeHandler}
+            id="myBox"
+            rows="8"
+          ></textarea>
+        </div>
 
-    <button className="btn btn-primary" onClick={onClickHandler} onChange={onChangeHandler}>{button}</button>
-    </div>
-  )
+        <button
+          className="btn btn-primary"
+          onClick={onClickHandler}
+          onChange={onChangeHandler}
+        >
+          Convert to uppercase
+        </button>
+        <button
+          className="btn btn-primary mx-2"
+          onClick={ClickHandler}
+          onChange={onChangeHandler}
+        >
+          Convert to lowercase
+        </button>
+        <button
+          className="btn btn-primary mx-2"
+          onClick={ClearHandler}
+          onChange={onChangeHandler}
+        >
+          Clear Text
+        </button>
+      </div>
+
+      <div className="container my-3"   style={{color: props.mode === "dark" ? "white" : "black"}}>
+        <h1>Your text summary</h1>
+        <p>
+          {text.split(" ").length} words and {text.length} characters
+        </p>
+        <p>{0.008 * text.split(" ").length} minutes read</p>
+        <h2>Preview</h2>
+        <p>{text.length>0?text:"Enter something in the textbox above to preview it here"}</p>
+      </div>
+    </>
+  );
 }
 
-export default TextFrom
+export default TextFrom;
